db3f8f546e6c572afea0d8bf47097df3bcce0d61
Sean Young <seanyoung247@gmail.com>
2024-07-03 19:51:00 +0100

    refactor: Changes Header component to use composition

--- frontend_source/src/App.tsx
+++ frontend_source/src/App.tsx
@@ -1,8 +1,11 @@
 
 import { Global, css, ThemeProvider } from '@emotion/react'
 import { themes, Theme } from './theme'
+import { ToggleTheme } from './components/ToggleTheme'
 import { useColorScheme } from './hooks/useColorScheme'
 import { Header } from './components/Header'
+
+import Logo from './assets/logo.svg?react'
 
 
 const pageStyles = (theme: Theme) => css`
@@ -13,22 +16,67 @@ const pageStyles = (theme: Theme) => css`
         background-color: ${theme.background};
         color: ${theme.foreground};
         font-family: ${theme.bodyFont};
-        
+    }
+`
+
+const headerStyles = (theme: Theme) => css`
+    /* flex: 1 1 0px; */
+    justify-content: flex-start;
+
+    & > * {
+        /* border-left: 1px solid red;
+        border-right: 1px solid red; */
+        /* flex: 1 1 0; */
+    }
+
+    .logo {
+        --foreground: ${theme.foreground};
+        --flash: ${theme.primaryAccent};
+        width: 48px;
+    }
+
+    h1 {
+        text-align: center;
+        font-family: ${theme.titleFont};
+    }
+
+    nav {
+        margin-left: auto;
+    }
+
+    nav > ul {
+        display: flex;
+        list-style: none;
+        justify-content: space-between;
+        align-items: center;
+        gap: 2em;
     }
 `
 
 const App = () => {
     const [ currentTheme, toggleTheme ] = useColorScheme();
+    const theme = themes[currentTheme]
 
     return (
         <ThemeProvider theme={themes[currentTheme]}>
-            <Global styles={(theme) => pageStyles(theme)} />
-            <Header toggleTheme={ toggleTheme } />
+            <Global styles={ pageStyles(theme) } />
+
+            <Header css={ headerStyles(theme) }>
+                <a><Logo className='logo'/></a>
+                <h1>Sean Young</h1>
+                <nav id="main-menu">
+                    <ul>
+                        <li><a>About</a></li>
+                        <li><a>Work</a></li>
+                        <li><a>Contact</a></li>
+                        <li><ToggleTheme toggle={ toggleTheme } /></li>
+                    </ul>
+                </nav>
+            </Header>
+
             <div css={{
                 height: '500vh'
-            }} >
-
-            </div>
+            }} />
             
         </ThemeProvider>
     )
--- frontend_source/src/components/Header.tsx
+++ frontend_source/src/components/Header.tsx
@@ -1,48 +1,34 @@
 
+import React from "react"
 import { useTheme, css } from "@emotion/react"
 import { Theme } from '../theme'
-import { ToggleTheme } from './ToggleTheme'
-import Logo from '../assets/logo.svg?react'
-import { SchemeToggler } from "../hooks/useColorScheme"
 
-type HeaderProps = {
-    toggleTheme: SchemeToggler
-}
 
 const headerStyles = (theme: Theme) => css`
+    position: fixed;
+    width: 100%;
+    padding: 0.5em;
+
     display: flex;
     gap: 1em;
     justify-content: space-between;
     align-items: center;
-    position: fixed;
-    width: 100%;
+
     background-color: ${theme.contentBackground};
     box-shadow: 0 0 5px ${theme.shadow};
-    padding: 0.5em;
-
     font-family: ${theme.titleFont};
-
-    .logo {
-        --foreground: ${theme.foreground};
-        --flash: ${theme.primaryAccent};
-        width: 48px;
-    }
-
-    h1 {
-        font-family: ${theme.titleFont};
-    }
 `
 
-export const Header = ({ toggleTheme }: HeaderProps) => {
+type HeaderProps = {
+    className?: string,
+    children: React.ReactNode,
+}
+
+export const Header = ({className, children}: HeaderProps) => {
     const theme = useTheme()
     return (<>
-        <header css={ headerStyles(theme) }>
-            <Logo className='logo'/>
-            <h1>Sean Young</h1>
-            About
-            Work
-            Contact
-            <ToggleTheme toggle={ toggleTheme } />
+        <header className={ className } css={ headerStyles(theme) }>
+            { children }
         </header>
         <div></div>
     </>)
