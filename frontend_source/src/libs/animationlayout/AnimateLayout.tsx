ba8cbe985e929ca03a78d0bdf125500b53d42555
Sean <seanyoung247@gmail.com>
2024-07-12 19:12:01 +0100

    refactor: code layout

--- frontend_source/src/libs/animationlayout/AnimateLayout.tsx
+++ frontend_source/src/libs/animationlayout/AnimateLayout.tsx
@@ -20,7 +20,6 @@ type ElementRef = React.MutableRefObject<HTMLElement>
 export const AnimateLayout = forwardRef<AnimateRef, AnimateLayoutProps>(
     ({offsets = calcOffsets, keyframes, position = null, options = {duration:100}, children}, elRef) => {
 
-
         const ref = useOptionalRef(elRef as ElementRef)
         const element = ref.current
         const previous = useRef<LayoutState>(getBlankState())
@@ -36,14 +35,16 @@ export const AnimateLayout = forwardRef<AnimateRef, AnimateLayoutProps>(
                     keyframes(offsets(oldLayout, newLayout)), 
                     options
                 )
+                // If a playback rate has been set in the options, set it. (allows pausing for instance)
                 previous.current.animation.playbackRate = options?.playbackRate ?? 1
+                // If a position has been provided, jump to it.
                 if (position) previous.current.animation.currentTime = position;
             }
         })
 
         return ( (Children.count(children) > 1) ?
             Children.map(children, (child, index) => (
-                <AnimateLayout key={ index } { ...{offsets, keyframes, position, options, children} }>
+                <AnimateLayout key={ index } { ...{offsets, keyframes, position, options} }>
                     { child }
                 </AnimateLayout>
             )) :
