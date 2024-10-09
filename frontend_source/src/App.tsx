7a6a275329d3d04122b8ad3ce2327951b1689885
sean <seanyoung247@gmail.com>
2024-10-09 16:30:34 +0100

    feat: Initial test app code

--- frontend_source/src/App.tsx
+++ frontend_source/src/App.tsx
@@ -1,75 +1,27 @@
+import { Plane3D } from "./components/Plane3D"
+import { View3D } from "./components/View3D"
 
-import { Global, css, ThemeProvider } from '@emotion/react'
-import { themes, Theme } from './theme'
-import { useColorScheme } from './hooks/useColorScheme'
-
-import { Header } from './components/header/header'
-import { scrollContext, useScrollState } from './hooks/useScroll'
-
-
-const pageStyles = (theme: Theme) => css`
-    * {
-        scrollbar-color: ${theme.foreground} ${theme.contentBackground};
-        transition: background 0.5s, foreground 0.5s, box-shadow 0.5s;
-    }
-    html, body {
-        background-color: ${theme.background};
-        color: ${theme.foreground};
-        font-family: ${theme.bodyFont};
-        scroll-behavior: smooth;
-    }
-    h1, h2, h3, h4, h5, h6 {
-        font-family: ${theme.titleFont};
-    }
-    a, a:visited {
-        color: ${theme.foreground};
-    }
-`
+import stone from './assets/stone.png'
+import { Coordinate3D } from "./utilities/coords"
 
 const App = () => {
-    const [ currentTheme, toggleTheme ] = useColorScheme();
-    const theme = themes[currentTheme]
-
-    const scroll = useScrollState()
-
     return (
-        <ThemeProvider theme={themes[currentTheme]}>
-            <scrollContext.Provider value={ scroll }>
-
-                <Global styles={ pageStyles(theme) } />
-
-                <Header toggleTheme={ toggleTheme } />
-
-                <main css={{
-                    // 'section:nth-of-type(odd)': {
-                    //     backgroundColor: theme.background,
-                    // },
-                    // 'section:nth-of-type(even)': {
-                    //     backgroundColor: theme.altBackground,
-                    // }
-                }}>
-                    <section id="about" css={{
-                        minHeight: '100lvh',
-                        paddingTop: '5em',
-                    }}>
-                        <h2>About</h2>
-                    </section>
-                    <section id="projects" css={{
-                        minHeight: '100lvh',
-                        paddingTop: '5em',
-                    }}>
-                        <h2>Projects</h2>
-                    </section>
-                    <section id="contact" css={{
-                        minHeight: '100lvh',
-                        paddingTop: '5em',
-                    }}>
-                        <h2>Contact</h2>
-                    </section>
-                </main>
-
-            </scrollContext.Provider>
-        </ThemeProvider>
+        <View3D width="100%" height="100lvh" perspective="600px">
+            <Plane3D 
+                width="600px" 
+                height="600px" 
+                position={ new Coordinate3D(0,0,-1000) } 
+                rotation={ new Coordinate3D(0,0,0) }
+                image={stone}
+            />
+            <Plane3D 
+                width="600px" 
+                height="600px" 
+                position={ new Coordinate3D(-300,0,-700) } 
+                rotation={ new Coordinate3D(0,90,0) }
+                image={stone}
+            />
+        </View3D>
     )
 }
 
